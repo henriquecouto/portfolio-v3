2919ca4d0cff5a4c8fd09265e45045815e1e5114
Henrique Couto <email@henriquecouto.com.br>
2020-09-29 20:53:12 -0300

    customized props to divider component

--- components/Divider/index.tsx
+++ components/Divider/index.tsx
@@ -1,16 +1,44 @@
 import { Divider as MuiDivider, DividerProps } from "@material-ui/core";
 import { makeStyles, Theme } from "@material-ui/core/styles";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme: Theme) => ({
-  divider: {
-    height: 6,
+  // divider: {
+  //   height: 6,
+  // },
+  primary: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  secondary: {
     backgroundColor: theme.palette.primary.main,
+  },
+  small: {
+    height: "2px",
+  },
+  high: {
+    height: "6px",
   },
 }));
 
-const Divider = (props: DividerProps) => {
+type Props = {
+  height?: "small" | "high";
+  background?: "primary" | "secondary";
+};
+
+const Divider = (props: DividerProps & Props) => {
+  const { height, background } = props;
   const classes = useStyles();
-  return <MuiDivider {...props} className={classes.divider} />;
+  return (
+    <MuiDivider
+      {...props}
+      className={clsx({
+        [classes.small]: height === "small",
+        [classes.high]: height === "high" || undefined,
+        [classes.primary]: background === "primary" || undefined,
+        [classes.secondary]: background === "secondary",
+      })}
+    />
+  );
 };
 
 export default Divider;
