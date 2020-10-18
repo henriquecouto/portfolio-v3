1dd730e103302b3fbe2bbb35f2b4d2874a664889
Henrique Couto <email@henriquecouto.com.br>
2020-10-18 14:26:05 -0300

    finish static contact form in home

--- components/Home/Contact/index.tsx
+++ components/Home/Contact/index.tsx
@@ -1,17 +1,54 @@
-import { Grid, Input, TextField, Typography } from "@material-ui/core";
+import {
+  Button,
+  Fab,
+  Grid,
+  TextField,
+  Theme,
+  Tooltip,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@iconify/react";
+import instagram from "@iconify/icons-jam/instagram";
+import github from "@iconify/icons-jam/github-square";
+import linkedin from "@iconify/icons-jam/linkedin-square";
 
 import Section from "../../Section";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  iconSocial: {
+    fontSize: 27,
+  },
+  buttonSocial: {
+    margin: theme.spacing(0, 2),
+    marginLeft: 0,
+  },
+}));
+
+const SocialButton: React.FC<{ network: string; icon: object }> = ({
+  network,
+  icon,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Tooltip title={network}>
+      <Fab color="primary" size="small" className={classes.buttonSocial}>
+        <Icon icon={icon} className={classes.iconSocial} />
+      </Fab>
+    </Tooltip>
+  );
+};
 
 const Contact: React.FC = () => {
   return (
     <Section title="Contato" color="secondary">
       <form noValidate autoComplete="off">
-        <Grid container spacing={2}>
+        <Grid container spacing={2} justify="space-between">
           <Grid item xs={12}>
             <TextField
               fullWidth
               label="Seu nome"
-              variant="filled"
+              variant="outlined"
               color="primary"
             />
           </Grid>
@@ -19,7 +56,7 @@ const Contact: React.FC = () => {
             <TextField
               fullWidth
               label="Seu email"
-              variant="filled"
+              variant="outlined"
               color="primary"
             />
           </Grid>
@@ -27,11 +64,22 @@ const Contact: React.FC = () => {
             <TextField
               fullWidth
               multiline
-              rows={7}
+              rows={6}
               label="Sua mensagem"
-              variant="filled"
+              variant="outlined"
               color="primary"
+              inputProps={{ maxLength: 500 }}
             />
+          </Grid>
+          <Grid item>
+            <SocialButton network="Github" icon={github} />
+            <SocialButton network="Linkedin" icon={linkedin} />
+            <SocialButton network="Instagram" icon={instagram} />
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="primary">
+              Enviar
+            </Button>
           </Grid>
         </Grid>
       </form>
