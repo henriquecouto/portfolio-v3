18c12a61eae52ae241ee036307210dcac3f86cd9
Henrique Couto <henrique.coutu@live.com>
2020-11-28 15:46:13 -0300

    back to use getInitialProps

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -1,7 +1,7 @@
 import { Container } from "@material-ui/core";
 import { Theme, makeStyles } from "@material-ui/core/styles";
 import Header from "../../components/Header";
-import { STATIC_URL } from "../../contants/urls";
+import { LOCAL_URL } from "../../contants/urls";
 import ReactMarkdown from "react-markdown/with-html";
 import toc from "remark-toc";
 import CodeBlock from "../../components/CodeBlock";
@@ -47,28 +47,10 @@ export default function WorkDetails({
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+WorkDetails.getInitialProps = async ({ query }) => {
   const works = await (
-    await fetch(`${STATIC_URL}/api/works?slug=${params.slug}`)
+    await fetch(`${LOCAL_URL}/api/works?slug=${query.slug}`)
   ).json();
-
-  const file = await (await fetch(STATIC_URL + works[0].content)).text();
-
-  return { props: { work: works[0], file } };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const works: Array<Work> = await (
-    await fetch(`${STATIC_URL}/api/works`)
-  ).json();
-
-  const paths = works.map((work) => {
-    return {
-      params: {
-        slug: work.slug,
-      },
-    };
-  });
-
-  return { paths, fallback: false };
+  const file = await (await fetch(LOCAL_URL + works[0].content)).text();
+  return { work: works[0], file };
 };
